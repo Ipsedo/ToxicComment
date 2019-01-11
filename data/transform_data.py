eae61e2d3af38bd595daa93af22be756c9a61c23
Ipsedo <berriens@gmail.com>
2019-01-11 01:04:40 +0100

    add pad sentence

--- data/transform_data.py
+++ data/transform_data.py
@@ -5,17 +5,20 @@ import numpy as np
 limit_count = 10
 
 
+padding = "<pad>"
+
+
 def split_comment(data):
     res = []
     for i, l in enumerate(data):
         ident = l[0]
         phrase = l[1]
-        res.append((ident, np.asarray(re.findall(r"[\w]+|[.,!?;']", phrase)))) # Sépare ponctution et mots de la séquence
+        res.append((ident, np.asarray(re.findall(r"[\w]+", phrase)))) # Sépare ponctution et mots de la séquence |[.,!?;']
     return res
 
 
 def mk_vocab(data):
-    vocab = {}
+    vocab = {padding: 0}
     count = {}
     for (ident, p) in data:
         for w in p:
@@ -25,13 +28,13 @@ def mk_vocab(data):
                 count[w] += 1
     for (ident, p) in data:
         for w in p:
-            if w not in vocab and count[w] >= limit_count:
+            if w not in vocab and count[w] > limit_count:
                 vocab[w] = len(vocab)
     return count, vocab
 
 
 def words_to_index(word_array, vocab, count):
-    return np.array([vocab[w] for w in word_array if count[w] >= limit_count])
+    return np.array([vocab[w] for w in word_array if count[w] > limit_count])
 
 
 def pass_data_to_word_idx(data, vocab, count):
@@ -42,3 +45,24 @@ def pass_data_to_word_idx(data, vocab, count):
         ident_sent.append(ident)
     return np.asarray(ident_sent), np.asarray(all_sent)
 
+
+def get_max_len_sent(sentences):
+    return max(map(len, sentences))
+
+
+def pad_sentences(sentences, max_len, pad_idx):
+    res = []
+    for s in sentences:
+        new_s = np.lib.pad(s, (0, max_len - s.shape[0]), 'constant', constant_values=(pad_idx,))
+        res.append(new_s)
+    return np.asarray(res)
+
+
+def filter_test_data(data, labels):
+    tmp = [(d, l) for d, l in zip(data, labels) if min(l) != -1]
+    res_data = []
+    res_labels = []
+    for d, l in tmp:
+        res_data.append(d)
+        res_labels.append(l)
+    return res_data, np.asarray(res_labels)
