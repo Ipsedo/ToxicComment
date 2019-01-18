4be70ff235dca6c07064bdef7b4b1e719541eafb
Ipsedo <berriens@gmail.com>
2019-01-18 11:42:46 +0100

    test max pool

--- model/conv_model.py
+++ model/conv_model.py
@@ -3,16 +3,19 @@ import torch.nn as nn
 
 class ConvModel(nn.Module):
 
-    def __init__(self, vocab_size, emb_size=50):
+    def __init__(self, vocab_size, sent_max_len, emb_size=16):
         super(ConvModel, self).__init__()
         self.emb = nn.Embedding(vocab_size, emb_size)
 
-        out_channel_conv1 = 25
+        out_channel_conv1 = 32
+        out_conv1_size = sent_max_len - 5 + 1
+        out_conv2_size = out_conv1_size - 3 + 1
         self.seq1 = nn.Sequential(
-            nn.Conv1d(emb_size, 25, 5),
+            nn.Conv1d(emb_size, 24, 5),
             nn.ReLU(),
-            nn.Conv1d(25, out_channel_conv1, 3),
-            nn.ReLU()
+            nn.Conv1d(24, out_channel_conv1, 3),
+            nn.ReLU(),
+            nn.MaxPool1d(out_conv2_size)
         )
         # TODO rajouter couche
 
@@ -28,8 +31,8 @@ class ConvModel(nn.Module):
         # TODO verifier enchainement shape
         out = self.emb(X)
         out = out.permute(0, 2, 1)
-        out = self.seq1(out)
+        out = self.seq1(out).squeeze(2)
         # somme sur tout les mots -> out.size() == (batch, out_channel_conv1)
-        out = out.mean(dim=2)
+        # out = out.mean(dim=2)
         out = self.seq2(out)
         return out
